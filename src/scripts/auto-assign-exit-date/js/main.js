a227c78cdc0217e630b07da64aaab1d00b7d4fff
Basiczombie <issacgable@gmail.com>
2016-05-11 08:31:41 -0600

    fixed for ps9

--- src/scripts/auto-assign-exit-date/js/main.js
+++ src/scripts/auto-assign-exit-date/js/main.js
@@ -1,7 +1,7 @@
 /*global require*/
 require.config({
     paths: {
-        autoassign: '/* @echo IMAGE_SERVER_URL */' + '/auto-assign-exit-date/js/autoassign'
+        autoassign: '/scripts/auto-assign-exit-date/js/autoassign'
     }
 });
 require(['autoassign'], function(autoassign) {
