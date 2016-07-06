5b32dba3f776f1e9c30bcad4784ae15d9b47f4cf
basiczombie <issacgable@gmail.com>
2016-07-06 07:26:55 -0600

    Fixed js to relect new standard

--- src/scripts/auto-assign-exit-date/js/autoassign.js
+++ src/scripts/auto-assign-exit-date/js/autoassign.js
@@ -1,13 +1,8 @@
-/*global $j, define, require*/
-
-define('autoassign', function () {
-    'use strict';
-    return {
-        insertExitDate: function () {
-            var exitDateField = $j('.psDateWidget').eq(1);
-            if (!exitDateField.val()) {
-                exitDateField.val(psData.exitDate);
-            }
-        }
-    };
-});
+(function($){
+  $(document).ready(function(){
+    var exitDateField = $('.psDateWidget').eq(1);
+    if(!exitDateField.val()){
+      exitDateField.val(psData.exitDate);
+    }
+  })
+})($j);
