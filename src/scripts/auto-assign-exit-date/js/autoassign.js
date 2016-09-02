054911b66fa8ccdaffe228e1921bb4f20d16e330
Nathan Jones <nathan.jones@ironmail.org>
2016-09-02 16:11:18 -0600

    Use PowerQuery to get exit date, and use es6 module syntax

--- src/scripts/auto-assign-exit-date/js/autoassign.js
+++ src/scripts/auto-assign-exit-date/js/autoassign.js
@@ -1,8 +1,25 @@
-(function($){
-  $(document).ready(function(){
-    var exitDateField = $('.psDateWidget').eq(1);
-    if(!exitDateField.val()){
-      exitDateField.val(psData.exitDate);
-    }
-  })
-})($j);
+import $ from 'jquery';
+
+export default function() {
+  $(() => {
+    fetch('/ws/schema/query/org.irondistrict.special-programs.exit-date.get', {
+        credentials: 'same-origin',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+      .then(resp => resp.json())
+      .then(exitDate => {
+        if (exitDate.record.length) {
+          const exitDateField = $('.psDateWidget').eq(1);
+          const lastDay = exitDate.record[0].lastday;
+          if (!exitDateField.val()) {
+            exitDateField.val(lastDay);
+            exitDateField.prop('disabled', 'disabled');
+          }
+        }
+      });
+  });
+}
