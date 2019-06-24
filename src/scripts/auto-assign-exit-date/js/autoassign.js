7214b16c3e7d35d16154162c25f1b440b555db3f
Issac Gable <issacgable@gmail.com>
2019-06-24 12:06:32 -0600

    Update for plugin deployment

--- src/scripts/auto-assign-exit-date/js/autoassign.js
+++ src/scripts/auto-assign-exit-date/js/autoassign.js
@@ -1,24 +1,22 @@
 import $ from 'jquery';
 
-export default function() {
-  $(() => {
-    fetch('/ws/schema/query/org.irondistrict.special-programs.exit-date.get', {
-        credentials: 'same-origin',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
+$(() => {
+  fetch('/ws/schema/query/org.irondistrict.special-programs.exit-date.get', {
+      credentials: 'same-origin',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(resp => resp.json())
+    .then(exitDate => {
+      if (exitDate.record.length) {
+        const exitDateField = $('.psDateWidget').eq(1);
+        const lastDay = exitDate.record[0].lastday;
+        if (!exitDateField.val()) {
+          exitDateField.val(lastDay);
         }
-      })
-      .then(resp => resp.json())
-      .then(exitDate => {
-        if (exitDate.record.length) {
-          const exitDateField = $('.psDateWidget').eq(1);
-          const lastDay = exitDate.record[0].lastday;
-          if (!exitDateField.val()) {
-            exitDateField.val(lastDay);
-          }
-        }
-      });
-  });
-}
+      }
+    });
+});
